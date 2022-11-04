d11e94020b83094b69940c99af3ddc8049dd84a3
amanda rojter <rojteramanda@gmail.com>
2022-11-04 13:34:47 -0400

    tweet quote and event listeners added

--- script.js
+++ script.js
@@ -36,6 +36,15 @@ async function getQuotes() {
   }
 }
 
+//tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // //on load
 getQuotes();
-newQuote();
