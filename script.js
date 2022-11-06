8ac61b3f6d5c2c03f66399859a0f879625e021e8
amanda rojter <rojteramanda@gmail.com>
2022-11-06 09:59:43 -0500

    added loading spinner

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 //DOM variables
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const newQuoteBtn = document.getElementById('new-quote');
 const twitterBtn = document.getElementById('twitter');
+const loader = document.querySelector('.loader');
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 let apiQuotes = [];
 
 //show new quote
 function newQuote() {
+  showLoadingSpinner();
   //pick a random quote from API quotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   quoteText.textContent = quote.text;
+  //reduce font size for longer quotes
   if (quote.text.length > 100) {
     quoteText.classList.add('long-quote');
   } else {
@@ -19,13 +33,15 @@ function newQuote() {
   }
   if (!quote.author) {
     authorText.textContent = 'Unknown';
-  } else {
-    authorText.textContent = quote.author;
   }
+  //set quote
+  authorText.textContent = quote.author;
+  removeLoadingSpinner();
 }
 
 //get quotes from API
 async function getQuotes() {
+  showLoadingSpinner();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
